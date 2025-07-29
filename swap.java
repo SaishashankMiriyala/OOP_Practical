1822ee5859739414ee4d4452389e2f8c7ddbe3db
SaishashankMiriyala <saishashank.miriyala@gmail.com>
2025-07-29 11:38:29 +0530

    Add files via upload

---
+++ swap.java
@@ -0,0 +1,20 @@
+package prgs;
+import java.util.scanner;
+public class swap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=3,b=5,temp;
+		System.out.println("value of a before swap: " +a);
+		System.out.println("value of b: " +b);
+
+		temp=a;
+		a=b;
+		b=temp;
+		System.out.println(" after changing value of a: " +a);
+		System.out.println("after changing value of b: " +b);
+		}
+		
+	
+
+}
